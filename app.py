608be3104dc88797e8d58cc6a1b9b3d32704970b
dolsen0 <derek.olsen@gmail.com>
2023-01-15 00:42:30 -0500

    text generated upon user prompt

--- app.py
+++ app.py
@@ -5,10 +5,12 @@ from secret import apiKey
 
 openai.api_key = apiKey
 
-prompt = "Let me begin by "
+prompt = input("\nwhat would you like to say?\n")
 model = "text-davinci-002"
 
-completion = openai.Completion.create(engine=model, prompt=prompt, max_tokens = 10)
 
-print(completion)
-print(prompt)
+response = openai.Completion.create(engine=model, prompt=prompt, max_tokens = 10)
+generated_text = response["choices"][0]["text"]
+
+# print(response)
+print(generated_text)
