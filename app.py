f85ecfbaea86d3750c4f74e1f2c82b076b7ce17e
dolsen0 <derek.olsen@gmail.com>
2023-01-15 01:10:14 -0500

    added mood into response for variety of messages. ie friendly, angry, etc.

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
-import os
 import openai
-
 from secret import apiKey
 
 openai.api_key = apiKey
 
-prompt = input("\nwhat would you like to say?\n")
+message = input("\nYes?\n")
+mood = "friendly" # funny, angry, sarcastic, silly, etc.
+prompt = f"generate a {mood} response to someone writing the prompt: {message}"
 model = "text-davinci-002"
 
 
-response = openai.Completion.create(engine=model, prompt=prompt, max_tokens = 10)
+response = openai.Completion.create(engine=model, prompt=prompt, max_tokens = 40)
 generated_text = response["choices"][0]["text"]
 
 # print(response)
